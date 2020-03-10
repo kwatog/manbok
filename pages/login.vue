091d35a84a5104690ffa73d14566f24270ee0c9c
Michael Miciano <mikoy@mbp.lan>
2020-03-10 08:19:22 +0800

    use tailwind starter pack from creativetim

--- pages/login.vue
+++ pages/login.vue
@@ -1,41 +1,123 @@
 <template>
-  <div class="home flex justify-center flex-col items-center align-middle mt-12">
-  <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-        Email
-      </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-      id="email" type="text" 
-      v-model="email"
-      placeholder="email">
-    </div>
-    <div class="mb-6">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-        Password
-      </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
-      id="password" 
-      v-model="password"
-      type="password" 
-      placeholder="******************">
-    </div>
-    <div class="flex items-center justify-between">
-      <button 
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
-      @click="doLogin"
-      type="button">
-        Sign In
-      </button>
-      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-        Forgot Password?
-      </a>
-    </div>
+  <div>
+    <main>
+      <section class="absolute w-full h-full">
+        <div
+          class="absolute top-0 w-full h-full bg-gray-900"
+          style="background-size: 100%; background-repeat: no-repeat;"
+          :style="{'background-image': 'url(' + require('../assets/img/register_bg_2.png') + ')'}"
+        ></div>
+        <div class="container mx-auto px-4 h-full">
+          <div class="flex content-center items-center justify-center h-full">
+            <div class="w-full lg:w-4/12 px-4">
+              <div
+                class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0"
+              >
+                <div class="rounded-t mb-0 px-6 py-6">
+                  <div class="text-center mb-3">
+                    <h6 class="text-gray-600 text-sm font-bold">
+                      Sign in with Email
+                    </h6>
+                  </div>
+                  <!-- div class="btn-wrapper text-center">
+                    <button
+                      class="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
+                      type="button"
+                      style="transition: all 0.15s ease 0s;"
+                    >
+                      <img
+                        alt="..."
+                        class="w-5 mr-1"
+                        src="../assets/img/github.svg"
+                      />Github</button
+                    ><button
+                      class="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
+                      type="button"
+                      style="transition: all 0.15s ease 0s;"
+                    >
+                      <img
+                        alt="..."
+                        class="w-5 mr-1"
+                        src="../assets/img/google.svg"
+                      />Google
+                    </button>
+                  </div>
+                  <hr class="mt-6 border-b-1 border-gray-400" / -->
+                </div>
+                <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+                  <!-- div class="text-gray-500 text-center mb-3 font-bold">
+                    <small>Or sign in with credentials</small>
+                  </div -->
+                  <form>
+                    <div class="relative w-full mb-3">
+                      <label
+                        class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        for="grid-password"
+                        >Email</label
+                      ><input
+                        type="email"
+                        v-model="email"
+                        class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        placeholder="Email"
+                        style="transition: all 0.15s ease 0s;"
+                      />
+                    </div>
+                    <div class="relative w-full mb-3">
+                      <label
+                        class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                        for="grid-password"
+                        >Password</label
+                      ><input
+                        type="password"
+                        v-model="password"
+                        class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                        placeholder="Password"
+                        style="transition: all 0.15s ease 0s;"
+                      />
+                    </div>
+                    <div>
+                      <label class="inline-flex items-center cursor-pointer"
+                        ><input
+                          id="customCheckLogin"
+                          type="checkbox"
+                          class="form-checkbox text-gray-800 ml-1 w-5 h-5"
+                          style="transition: all 0.15s ease 0s;"
+                        /><span class="ml-2 text-sm font-semibold text-gray-700"
+                          >Remember me</span
+                        ></label
+                      >
+                    </div>
+                    <div class="text-center mt-6">
+                      <button
+                        class="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
+                        type="button"
+                        style="transition: all 0.15s ease 0s;"
+                        @click="doLogin"
+                      >
+                        Sign In
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div class="flex flex-wrap mt-6">
+                <div class="w-1/2">
+                  <a href="#pablo" class="text-gray-300"
+                    ><small>Forgot password?</small></a
+                  >
+                </div>
+                <div class="w-1/2 text-right">
+                  <a href="#pablo" class="text-gray-300"
+                    ><small>Create new account</small></a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   </div>
-  <p class="text-center text-gray-500 text-xs">
-    &copy;2020 Acme Corp. All rights reserved.
-  </p>
-</div>
 </template>
 
 <script>
