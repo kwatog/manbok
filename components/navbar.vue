f1efa858cc44d443af4385dc3e1d319f8eb76258
Michael Miciano <mikoy@mbp.lan>
2020-03-13 07:51:12 +0800

    vuex

---
+++ components/navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav
+    class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg"
+  >
+  <div
+        class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
+  >
+    <button
+      class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+      type="button"
+      v-on:click="toggleNavbar()"
+    >
+      <i class="fas fa-bars"></i>Menu
+    </button>
+  </div>
+  <a href="/login">Login</a>
+  <span>Logout</span>
+  </nav>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      showMenu: false
+    }
+  },
+  methods: {
+    toggleNavbar: function(){
+      this.showMenu = !this.showMenu;
+    }
+  }
+}
+</script>
---
+++ store/login.js
@@ -0,0 +1,64 @@
+export const state = () => ({
+    authUser: null
+})
+
+export const mutations = {
+  SET_AUTH_USER (state, user) {
+    state.authUser = {
+      uid: user.uid,
+      email: user.email
+    }
+  }
+}
+
+export const actions = {
+  handleSuccessfulAuthentication({ commit }, { authUser, claims }) {
+    const _this = this
+    // Install servicerWorker if supported on sign-in/sign-up page.
+    // if (process.browser && 'serviceWorker' in navigator) {
+    //    navigator.serviceWorker.register('/firebase-auth-sw.js', { scope: '/' })
+    // }
+    _this.$fireAuth.currentUser.getIdToken(true).then((idToken) => {
+      _this.$axios.setHeader('Authorization', `JWT ${idToken}`)
+    }).catch((error) => {
+      throw new Error('An Error Ocurred: ', error)
+    })
+
+    commit('SET_AUTH_USER', { authUser })
+    console.log(authUser)
+  },    
+  async logout ({ commit }) {
+    await auth.signOut();
+    commit('SET_AUTH_USER', null);
+  },
+
+  async register ({ commit }, { email, password }) {
+    try {
+      await auth.createUserWithEmailAndPassword(email, password);
+      const user = auth.currentUser;
+      await user.sendEmailVerification();
+      commit('SET_AUTH_USER', user.user);
+    } catch (error) {
+        if(error.code === 'auth/email-already-in-use') {
+          try {
+            const user = await this.$fireAuth.signInWithEmailAndPassword(this.email, this.password)
+            commit('SET_AUTH_USER', user.user);
+          } catch (error) {
+            throw new Error('An Error Ocurred: ', error);
+          }
+        } else {
+          throw new Error('An Error Ocurred: ', error);
+        }
+      }
+    },
+
+
+  async loginGithub({ commit }) {
+    try {
+      const user = await auth.signInWithPopup(github);
+      commit('SET_AUTH_USER', user.user);
+      } catch (error) {
+      throw new Error('An Error Ocurred: ', error);
+    }
+  }
+}
